26eeb162e9e6b9cf883067494c0c7f81f6763b87
Shajeel <shajeelriaz65@gmail.com>
2024-10-31 03:21:00 +0500

    Update email.service.ts

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class EmailService {
+    private readonly logger = new Logger(EmailService.name);
     constructor(private readonly mailerService: MailerService) {}
 
     async sendEmail(to: string, subject: string, text: string) {
+        try {
         await this.mailerService.sendMail({
             to,
             subject,
             text
         });
+            this.logger.log(`Email sent successfully to ${to}`);
+        } catch (error) {
+            this.logger.error(`Failed to send email to ${to}`, error);
+        }
     }
 }
